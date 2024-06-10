4149716218b5d4b8e15577d2a6e7e62b416bf3c5
laloosun <lalu.sandu@gmail.com>
2024-06-10 17:44:02 +0100

    User class

--- ExpenseTracker/src/main/java/com/expenses/tracker/User.java
+++ ExpenseTracker/src/main/java/com/expenses/tracker/User.java
@@ -1,4 +1,21 @@
-package com.expenses.tracker;
+import jakarta.persistence.*;
 
+import javax.management.relation.Role;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+
+    private Set<Role> roles;
+
 }

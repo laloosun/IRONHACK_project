dcc70c91fa2dbe56e2a25f63777632b8fbc7a88d
laloosun <lalu.sandu@gmail.com>
2024-06-15 11:25:48 +0100

    Account service 1 update

---
+++ ExpenseTracker/src/main/java/dev/v5tracker/AccountService.java
@@ -0,0 +1,21 @@
+package dev.v5tracker;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AccountService {
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+    public List<Account> findAll() {
+        return accountRepository.findAll();
+
+        public Account save(Account account) {
+            return accountRepository.save(account);
+        }
+    }
+}

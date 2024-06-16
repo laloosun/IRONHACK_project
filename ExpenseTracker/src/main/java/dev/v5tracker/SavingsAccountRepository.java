a7ea43d986512b8e6999ac1d37f15384de9f0b91
laloosun <lalu.sandu@gmail.com>
2024-06-16 07:57:27 +0100

    Savings Account Repo update

---
+++ ExpenseTracker/src/main/java/dev/v5tracker/SavingsAccountRepository.java
@@ -0,0 +1,9 @@
+package dev.v5tracker;
+
+
+import org.springframework.stereotype.Repository;
+
+// Spring Data JPA Repository
+@Repository
+public interface SavingsAccountRepository extends AccountRepository {
+}

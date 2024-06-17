aa821fd29f86459fba121f69a8483002ee6b7117
laloosun <lalu.sandu@gmail.com>
2024-06-17 11:35:21 +0100

    checking repo update

--- ExpenseTracker/src/main/java/dev/v5tracker/CheckingAccountRepository.java
+++ ExpenseTracker/src/main/java/dev/v5tracker/CheckingAccountRepository.java
@@ -2,6 +2,7 @@ package dev.v5tracker;
 
 import org.springframework.stereotype.Repository;
 
+// Spring Data JPA Repository
 @Repository
 public interface CheckingAccountRepository extends AccountRepository {
 }

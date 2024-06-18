65c4c52f619ccb39714eea04244ca1ddf89fad3a
laloosun <lalu.sandu@gmail.com>
2024-06-18 12:51:06 +0100

    ExpenseTrackerApplication1

--- ExpenseTracker/src/main/java/dev/v5tracker/ExpenseTrackerApplication.java
+++ ExpenseTracker/src/main/java/dev/v5tracker/ExpenseTrackerApplication.java
@@ -1,7 +1,14 @@
 package dev.v5tracker;
 
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class ExpenseTrackerApplication {
@@ -9,4 +16,3 @@ public class ExpenseTrackerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ExpenseTrackerApplication.class, args);
     }
-}

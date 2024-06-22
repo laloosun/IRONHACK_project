86cf009d8b231400f74a7c0e4b822094bcd8a144
laloosun <lalu.sandu@gmail.com>
2024-06-22 12:10:19 +0100

    SecurityConfig2

--- ExpenseTracker/src/main/java/dev/v5tracker/SecurityConfig.java
+++ ExpenseTracker/src/main/java/dev/v5tracker/SecurityConfig.java
@@ -2,10 +2,13 @@ package dev.v5tracker;
 
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -14,5 +17,11 @@ public class SecurityConfig {
 
     private final UserDetailsService userDetailsService;
     private final AuthenticationManagerBuilder authManagerBuilder;
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
 
 }

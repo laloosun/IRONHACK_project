76f323d087d33066976839034b7272e6a37c13f2
laloosun <lalu.sandu@gmail.com>
2024-06-22 12:14:13 +0100

    SecurityConfig5

--- ExpenseTracker/src/main/java/dev/v5tracker/SecurityConfig.java
+++ ExpenseTracker/src/main/java/dev/v5tracker/SecurityConfig.java
@@ -14,6 +14,8 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
@@ -36,5 +38,8 @@ public class SecurityConfig {
     protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authManagerBuilder.getOrBuild());
         customAuthenticationFilter.setFilterProcessesUrl("/api/login");
-
-}
+        
+        http.csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(STATELESS);
+        
+    }

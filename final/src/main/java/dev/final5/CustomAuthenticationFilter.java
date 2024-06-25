9b86eb5464c4246d869761f47be58468f58cbdae
laloosun <lalu.sandu@gmail.com>
2024-06-25 09:07:31 +0100

    CustomAuth4

--- final/src/main/java/dev/final5/CustomAuthenticationFilter.java
+++ final/src/main/java/dev/final5/CustomAuthenticationFilter.java
@@ -55,3 +55,23 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
     }
 
 
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) throws IOException, ServletException {
+        User user = (User) authentication.getPrincipal();
+        Algorithm algorithm = Algorithm.HMAC256(jwtSecret.getBytes());
+
+        String access_token = JWT.create()
+                .withSubject(user.getUsername())
+                .withExpiresAt(new Date(System.currentTimeMillis() + 10 * 60 * 1000))
+                .withIssuer(request.getRequestURL().toString())
+                .withClaim("roles", user.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
+                .sign(algorithm);
+
+        Map<String, String> tokens = new HashMap<>();
+        tokens.put("access_token", access_token);
+
+        response.setContentType(APPLICATION_JSON_VALUE);
+        new ObjectMapper().writeValue(response.getOutputStream(), tokens);
+    }
+}
+

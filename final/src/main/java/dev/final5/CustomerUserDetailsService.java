778567262c0fb586a93474102d0eefb630d9e61b
laloosun <lalu.sandu@gmail.com>
2024-06-29 15:53:08 +0100

    CustUserdet5

--- final/src/main/java/dev/final5/CustomerUserDetailsService.java
+++ final/src/main/java/dev/final5/CustomerUserDetailsService.java
@@ -15,19 +15,15 @@ import java.util.stream.Collectors;
 public class CustomUserDetailsService implements UserDetailsService {
     private final UserRepository userRepository;
 
-
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByUsername(username);
-
         if (user == null) {
             throw new UsernameNotFoundException("User not found");
         }
-
         return new org.springframework.security.core.userdetails.User(
                 user.getUsername(),
                 user.getPassword(),
-
                 user.getRoles().stream()
                         .map(role -> new SimpleGrantedAuthority(role.getName()))
                         .collect(Collectors.toList())
